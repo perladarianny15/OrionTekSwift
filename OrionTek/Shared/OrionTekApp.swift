a50cbd0d7f4fa78a419abd0aed53f43f989481d1
Perla <perlad0015@gmail.com>
2021-06-23 21:52:30 -0400

    initializing swift app

---
+++ OrionTek/Shared/OrionTekApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrionTekApp.swift
+//  Shared
+//
+//  Created by Perla Moreno de jesus on 23/6/21.
+//
+
+import SwiftUI
+
+@main
+struct OrionTekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
