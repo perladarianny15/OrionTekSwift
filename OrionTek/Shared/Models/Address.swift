5b9993fcb1afe905575a6e255fa7bc06de2c7e45
Perla <perlad0015@gmail.com>
2021-06-26 15:11:10 -0400

    deleting unnecesary comments

--- OrionTek/Shared/Models/Address.swift
+++ OrionTek/Shared/Models/Address.swift
@@ -18,31 +18,3 @@ class Address: Identifiable, Codable
     public var street: String?
     public var streetNumber: String?
 }
-
-//class DbAddress: Codable
-//{
-//    var id: Int
-//    var country: String?
-//    var city: String?
-//    var zipcode: String?
-//    var street: String?
-//    var streetNumber: String?
-//
-//    var list : [Address]?
-//}
-//
-//class dbClient: Codable {
-//    var id: Int = 1
-//    var name: String?
-//    var lastname: String?
-//    var email: String?
-//    var number : String?
-//
-//    var Address : [Address]?
-//}
-
-
-struct Contact {
-  let id: Int32
-  let name: NSString
-}
